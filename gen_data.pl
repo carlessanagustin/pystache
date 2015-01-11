01378a07d84123d39a2fae5a213a40a40a754e40
Carles San Agustin <info@carlessanagustin.com>
2015-01-11 23:54:59 +0100

    perl data generatos

---
+++ gen_data.pl
@@ -0,0 +1,8 @@
+#!/usr/bin/perl
+my $workspace = $ENV{'WORKSPACE'};
+
+open(P1, ">$workspace/hits.properties")|| die;
+open(P2, ">$workspace/misses.properties")|| die;
+
+print P1 "YVALUE=".rand(100);
+print P2 "YVALUE=".rand(50);
